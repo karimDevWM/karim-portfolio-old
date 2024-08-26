238e9948bdc443f1c95518808cd1a941dd88e94f
devjava69 <k.trabelsi9@laposte.net>
2024-08-26 16:14:46 +0100

    commit

---
+++ karim-portfolio/src/app/header/header.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslationLoaderService } from '../service/translation-loader.service';
+import { TranslateService } from '@ngx-translate/core';
+import { locale as english } from '../shared/i18n/en';
+import { locale as french } from '../shared/i18n/fr';
+import $ from 'jquery';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit {
+  language:any="";
+  constructor(private _translationLoaderService: TranslationLoaderService,private _translateService: TranslateService) {
+    this._translationLoaderService.loadTranslations(english, french);
+    if(localStorage.getItem("lang")){
+      this.language=localStorage.getItem("lang");
+    }
+    else{
+      this.language='fr';
+    }
+  }
+
+  languageChange($event:any){
+    let lang=$event.value;
+    localStorage.setItem("lang", lang);
+    this._translateService.setDefaultLang(lang);
+    this._translateService.use(lang);
+  }
+
+  ngOnInit(): void {
+    $('.js-scroll-trigger').on('click',
+      function (): void {
+        $('.navbar-collapse').toggle();
+      }
+    );
+    $('.nav').on('click',
+      function (): void {
+        $('.navbar-collapse').toggle();
+      }
+    );
+  }
+}
